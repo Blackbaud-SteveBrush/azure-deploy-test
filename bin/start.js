fb6c71e82cf8621d6003674db9b4191a8163eb57
stevebrush <steve.brush@blackbaud.com>
2016-04-08 11:30:45 -0400

    Updated start script

--- bin/start.js
+++ bin/start.js
@@ -1,1 +1,19 @@
-require('../index.js');
+(function () {
+    'use strict';
+
+    var dotenv,
+        fs;
+
+    fs = require('fs');
+    dotenv = require('dotenv');
+
+    try {
+        if (fs.statSync('config.env').isFile()) {
+            dotenv.config({
+                path: 'config.env'
+            });
+        }
+    } catch (e) {}
+
+    require('../index.js');
+}());
