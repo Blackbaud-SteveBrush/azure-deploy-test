09d5701e54f0f97395c9c1de9417534ad0bdfb12
stevebrush <steve.brush@blackbaud.com>
2016-04-08 10:21:05 -0400

    Added setup step

--- deploy.sh
+++ deploy.sh
@@ -77,7 +77,7 @@ selectNodeVersion () {
       NODE_EXE=`cat "$DEPLOYMENT_TEMP/__nodeVersion.tmp"`
       exitWithMessageOnError "getting node version failed"
     fi
-    
+
     if [[ -e "$DEPLOYMENT_TEMP/__npmVersion.tmp" ]]; then
       NPM_JS_PATH=`cat "$DEPLOYMENT_TEMP/__npmVersion.tmp"`
       exitWithMessageOnError "getting npm version failed"
@@ -114,6 +114,8 @@ if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd "$DEPLOYMENT_TARGET"
   eval $NPM_CMD install --production
   exitWithMessageOnError "npm failed"
+  eval $NPM_CMD run setup
+  exitWithMessageOnError "npm setup failed"
   cd - > /dev/null
 fi
 
